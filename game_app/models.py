e5e4b7d8da0dcb503527621cc5b050565fcc1e0a
oleh_shmidt <shmidtua@gmail.com>
2023-10-27 15:20:26 +0300

    added  process repply method to DB and logic to check the correct reply

--- game_app/models.py
+++ game_app/models.py
@@ -10,6 +10,15 @@ class Word(models.Model):
     masked_word = models.CharField(max_length=100, blank=True, null=True)
     number_of_letter = models.IntegerField(null=True, blank=True)
     letter = models.CharField(max_length=1, blank=True, null=True)
+    success = models.CharField(
+        max_length=100,  # Adjust the max_length as needed
+        default="You have guessed it!"
+        )
+    failure = models.CharField(
+        max_length=100,  # Adjust the max_length as needed
+        default="Try again..."
+        )
+
 
     def __str__(self):
         return self.word
@@ -33,3 +42,9 @@ class Word(models.Model):
 
         self.masked_word = self.hidden_string
         self.save()
+
+    def process_reply(self):
+        if self.word[self.number_of_letter -1] == self.letter:
+            return self.success
+        else:
+            return self.failure
