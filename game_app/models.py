0a1b3e49d510bc380c9c881af32a6e1710c1157a
oleh_shmidt <shmidtua@gmail.com>
2023-12-08 09:14:38 +0200

    deleted sting fields from word model

--- game_app/models.py
+++ game_app/models.py
@@ -24,59 +24,12 @@ class Word(models.Model):
     #Defaul feilds for each user
     word = models.TextField()
     description = models.TextField()
-    success = models.CharField(
-        max_length=100,  # Adjust the max_length as needed
-        default="You have guessed it!"
-        )
-    failure = models.CharField(
-        max_length=100,  # Adjust the max_length as needed
-        default="Try again..."
-        )
-
-    call_to_action = models.CharField(
-        max_length=100,  # Adjust the max_length as needed
-        default="Ok make your choise now!"
-        )
-    you_win = models.CharField(
-        max_length=100,  # Adjust the max_length as needed
-        default="Congrats! You have GUESSED IT!!!!"
-)
-
-    game_over = models.CharField(
-        max_length=100,  # Adjust the max_length as needed
-        default="Game Over. Try again!"
-)
-
-    # number_of_letter = models.IntegerField(null=True, blank=True)
-    # letter = models.CharField(max_length=1, blank=True, null=True)
-
-    guessed = models.BooleanField(default=False)
 
     def __str__(self):
         return self.word
 
 
-    # def process_reply(self):
-    #     if self.number_of_letter and self.letter:
-    #         if self.word[self.number_of_letter -1] == self.letter:
-    #             temp_list = list(self.masked_word)
-    #             temp_list[self.number_of_letter -1] = self.letter
-    #             self.masked_word = "".join(temp_list)
-    #             self.save()
-    #             if self.word == self.masked_word:
-    #                 return self.you_win
-    #             else:
-    #                 return self.success
-    #
-    #         elif self.number_of_attempts == 1:
-    #
-    #             return self.game_over
-    #         else:
-    #             return self.failure
-    #     else:
-    #         if not self.number_of_letter and not self.letter:
-    #             #self.save()
-    #             return self.call_to_action
+
 
 
 #Creating a user profile when the user is registered
