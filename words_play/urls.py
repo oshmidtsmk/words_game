22433c602b90d4e2eb41e9eded0931bc6e9b1c3d
oleh_shmidt <shmidtua@gmail.com>
2023-12-14 09:31:29 +0200

    added stytlin of index, welcome, user words

--- words_play/urls.py
+++ words_play/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
     path('accounts/', include('accounts.urls', namespace = 'accounts')),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('test/', views.TestPage.as_view(), name = 'test'),
+    path('welcome/', views.TestPage.as_view(), name = 'test'),
     path('thanks/', views.ThanksPage.as_view(), name = 'thanks'),
     path('game/', include('game_app.urls', namespace = 'game')),
 
