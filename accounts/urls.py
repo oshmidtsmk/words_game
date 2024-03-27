e02011f04212e9d3f85ab8b5146b47b29c5427d6
oleh_shmidt <shmidtua@gmail.com>
2024-03-27 08:41:01 +0200

    added first version of resetting password with default django views

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,6 +4,7 @@ from django.urls import path, include
 from django.contrib.auth import views as auth_views # is automatically providing build in login/logout views.
 from accounts import views
 
+
 app_name = 'accounts'
 
 
@@ -12,5 +13,10 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name = 'logout'),# it goes to homepahe
     path('signup/', views.SignUp.as_view(), name = 'signup'),
     #path('oauth/', include('social_django.urls', namespace='social')),  #FB
+    #for password resetting
+    path("password_reset/", auth_views.PasswordResetView.as_view(), name="password_reset"),
+    path("password_reset/done/",auth_views.PasswordResetDoneView.as_view(),name="password_reset_done"),
+    path("reset/<uidb64>/<token>/",auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path("reset/done/",auth_views.PasswordResetCompleteView.as_view(),name="password_reset_complete"),
 
 ]
--- words_play/settings.py
+++ words_play/settings.py
@@ -15,6 +15,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from django.utils.translation import gettext_lazy as _
+from django.core.mail import send_mail #for resetting password
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -183,3 +184,11 @@ LOGOUT_REDIRECT_URL = "thanks"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#SMTP configuration:
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587  # Port should be an integer, not a string
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'shmidtoleh728@gmail.com'
+EMAIL_HOST_PASSWORD = 'fgwo zohz sqnh temb'
